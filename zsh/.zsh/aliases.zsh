68d54fd6b0bd737c42a05fa4da147eb53fcf6c97
Gabriel G. Lescinskas <gabriel@keplar.nl>
2015-10-12 23:18:23 +0200

    Update quote styling

--- zsh/.zsh/aliases.zsh
+++ zsh/.zsh/aliases.zsh
@@ -12,20 +12,17 @@ alias pdbreset="powder stop && bundle exec rake db:drop db:create db:migrate SEE
 # GIT
 #
 
-alias ga='git add'
-alias gaa='git add --all'
-alias gb='git branch'
-alias gc='git commit -v'
-alias gcm='git checkout master'
-alias gcb='git checkout -b'
-alias gco='git checkout'
-alias gcp='git cherry-pick'
-alias gfa='git fetch --all --prune'
-
-alias ggpull='git pull origin $(current_branch)'
-
-
-alias ggpush='git push origin $(current_branch)'
+alias ga="git add"
+alias gaa="git add --all"
+alias gb="git branch"
+alias gc="git commit -v"
+alias gcm="git checkout master"
+alias gcb="git checkout -b"
+alias gco="git checkout"
+alias gcp="git cherry-pick"
+alias gfa="git fetch --all --prune"
+alias ggpull="git pull origin $(current_branch)"
+alias ggpush="git push origin $(current_branch)"
 
 
 ggf() {
@@ -42,7 +39,7 @@ git pull origin "${b:=$1}"
 fi
 }
 
-alias ggpull='git pull origin $(current_branch)'
+alias ggpull="git pull origin $(current_branch)"
 
 ggp() {
 if [[ "$#" != 0 ]] && [[ "$#" != 1 ]]; then
@@ -53,7 +50,7 @@ git push origin "${b:=$1}"
 fi
 }
 
-alias ggpush='git push origin $(current_branch)'
+alias ggpush="git push origin $(current_branch)"
 
 ggpnp() {
 if [[ "$#" == 0 ]]; then
@@ -68,31 +65,31 @@ ggu() {
 git pull --rebase origin "${b:=$1}"
 }
 
-alias ggpur='ggu'
+alias ggpur="ggu"
 
 alias glol="git log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
-alias gm='git merge'
+alias gm="git merge"
 
-alias grb='git rebase'
-alias grba='git rebase --abort'
-alias grbc='git rebase --continue'
-alias grbi='git rebase -i'
-alias grbm='git rebase master'
-alias grbs='git rebase --skip'
-alias gst='git status'
+alias grb="git rebase"
+alias grba="git rebase --abort"
+alias grbc="git rebase --continue"
+alias grbi="git rebase -i"
+alias grbm="git rebase master"
+alias grbs="git rebase --skip"
+alias gst="git status"
 
 alias gfix="git commit --fixup"
-alias gfixd='git commit --fixup $(git rev-parse HEAD)'
+alias gfixd="git commit --fixup $(git rev-parse HEAD)"
 alias gsq="git commit --squash"
 
 #
 # HomeBrew
 #
 
-alias brews='brew list -1'
-alias bubo='brew update && brew outdated'
-alias bubc='brew upgrade && brew cleanup'
-alias bubu='bubo && bubc'
+alias brews="brew list -1"
+alias bubo="brew update && brew outdated"
+alias bubc="brew upgrade && brew cleanup"
+alias bubu="bubo && bubc"
 
 #
 # Other
