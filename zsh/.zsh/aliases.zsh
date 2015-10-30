f70ba949ddbbdf7cafa572615f3c1e130ccdf46c
Gabriel G. Lescinskas <gabriel@keplar.nl>
2015-10-30 15:16:53 +0100

    Update git aliases

--- zsh/.zsh/aliases.zsh
+++ zsh/.zsh/aliases.zsh
@@ -21,62 +21,25 @@ alias gcb="git checkout -b"
 alias gco="git checkout"
 alias gcp="git cherry-pick"
 alias gfa="git fetch --all --prune"
-alias ggpull="git pull origin $(current_branch)"
-alias ggpush="git push origin $(current_branch)"
-
-
-ggf() {
-[[ "$#" != 1 ]] && local b="$(current_branch)"
-git push --force origin "${b:=$1}"
-}
-
-ggl() {
-if [[ "$#" != 0 ]] && [[ "$#" != 1 ]]; then
-git pull origin "${*}"
-else
-[[ "$#" == 0 ]] && local b="$(current_branch)"
-git pull origin "${b:=$1}"
-fi
-}
+alias ggsup="git branch --set-upstream-to=origin/$(current_branch)"
 
 alias ggpull="git pull origin $(current_branch)"
-
-ggp() {
-if [[ "$#" != 0 ]] && [[ "$#" != 1 ]]; then
-git push origin "${*}"
-else
-[[ "$#" == 0 ]] && local b="$(current_branch)"
-git push origin "${b:=$1}"
-fi
-}
-
 alias ggpush="git push origin $(current_branch)"
-
-ggpnp() {
-if [[ "$#" == 0 ]]; then
-ggl && ggp
-else
-ggl "${*}" && ggp "${*}"
-fi
-}
-
-ggu() {
-[[ "$#" != 1 ]] && local b="$(current_branch)"
-git pull --rebase origin "${b:=$1}"
-}
-
 alias ggpur="ggu"
 
-alias glol="git log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
-alias gm="git merge"
 
+alias gst="git status"
+alias glol="git log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
+
+
+alias gm="git merge"
 alias grb="git rebase"
 alias grba="git rebase --abort"
 alias grbc="git rebase --continue"
 alias grbi="git rebase -i"
 alias grbm="git rebase master"
 alias grbs="git rebase --skip"
-alias gst="git status"
+
 
 alias gfix="git commit --fixup"
 alias gsq="git commit --squash"
