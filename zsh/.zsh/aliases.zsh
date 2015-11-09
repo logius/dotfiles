29e3ff3eebe01451883607143a8f97eebcc43c3c
Gabriel G. Lescinskas <gabriel@keplar.nl>
2015-11-09 15:11:38 +0100

    Update aliases for db reset commands

--- zsh/.zsh/aliases.zsh
+++ zsh/.zsh/aliases.zsh
@@ -5,8 +5,8 @@
 alias dbstart="pg_ctl -D /usr/local/var/postgres -l /usr/local/var/postgres/server.log start"
 alias dbstop="pg_ctl -D /usr/local/var/postgres stop -s -m fast"
 
-alias dbreset="bundle exec rake db:drop db:create db:migrate SEED_DEMO=1 db:seed"
-alias pdbreset="powder stop && bundle exec rake db:drop db:create db:migrate SEED_DEMO=1 db:seed && powder start"
+alias dbreset="bundle exec rake db:migrate:reset && SEED_DEMO=1 bundle exec rake db:seed"
+alias pdbreset="powder stop && bundle exec rake db:migrate:reset && SEED_DEMO=1 bundle exec rake db:seed && powder start"
 
 #
 # GIT
